ca1dc50cb9fc7f0411c07d2f3f12c6ac21265d2f
TheBenBro <54993958+TheBenBro@users.noreply.github.com>
2024-02-09 12:06:13 -0500

    no message

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,13 +9,23 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private Rigidbody2D playerRigidbody_;
     [SerializeField] private float acceleration_;
     [SerializeField] private float maxSpeed_;
+    [SerializeField] private float jumpPower_;
+    [SerializeField] private int maxAmountOfJumps_;
+    [SerializeField] private Transform shield_;
 
+    private Vector3 mousePosition_;
+    public int amountOfJumps_;
     private float speed_;
+    private bool isGrounded_;
 
+
+    private void Awake()
+    {
+        amountOfJumps_ = maxAmountOfJumps_;
+    }
     public void StartMovePlayer(InputAction.CallbackContext context)
     {
         Debug.Log(context.ReadValue<float>());
-
     }
 
     public void OnMove(InputValue value)
@@ -26,7 +36,20 @@ public class PlayerMovement : MonoBehaviour
 
     public void OnJump(InputValue value)
     {
-        playerRigidbody_.AddForce(new Vector2(playerRigidbody_.velocity.normalized.x, 5), ForceMode2D.Impulse);
+        if (amountOfJumps_ > 0)
+        {
+            playerRigidbody_.AddForce(new Vector2(playerRigidbody_.velocity.normalized.x, jumpPower_), ForceMode2D.Impulse);
+            amountOfJumps_--;
+        }
+    }
+
+    private void Update()
+    {
+        mousePosition_ = Mouse.current.position.ReadValue();
+        Vector3 worldMousePosition = Camera.main.ScreenToWorldPoint(new Vector3(mousePosition_.x, mousePosition_.y, Camera.main.nearClipPlane));
+        worldMousePosition.z = 0f;
+        // Make the transform look at the mouse position
+        shield_.right = worldMousePosition - transform.position;
     }
 
     private void FixedUpdate()
@@ -36,11 +59,18 @@ public class PlayerMovement : MonoBehaviour
             playerRigidbody_.velocity = Vector2.ClampMagnitude(playerRigidbody_.velocity, maxSpeed_);
         }
         playerRigidbody_.AddForce(new Vector2(speed_, playerRigidbody_.velocity.y), ForceMode2D.Force);
-
     }
 
-    //private void OnCollisionEnter2D(Collision2D collision)
-    //{
-    //    Debug.Log("hit");
-    //}
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (!collision.gameObject.CompareTag("Ground")) return;
+        amountOfJumps_ = maxAmountOfJumps_;
+        isGrounded_ = true;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (!collision.gameObject.CompareTag("Ground")) return;
+        isGrounded_ = false;
+    }
 }
